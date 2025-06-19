84f8b772139cadebab0234c797fda2d3be55334d
Lyudmila <lyudmilabravo@gmail.com>
2025-06-19 20:28:59 +0700

    Задания классы и исключения

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,95 +1,97 @@
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = 100;
-        this.type = null;
-    }
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
 
-    fix() {
-        this.state = this.state * 1.5;
-    }
+	fix() {
+		this.state = this.state * 1.5;
+	}
 
-    set state(newState) {
-        if (newState <= 0) {
-            this._state = 0;
-        } else if (newState >= 100) {
-            this._state = 100;
-        } else {
-            this._state = newState;
-        }
-    }
+	set state(newState) {
+		if (newState <= 0) {
+			this._state = 0;
+		} else if (newState >= 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
 
-    get state() {
-        return this._state;
-    }
+	get state() {
+		return this._state;
+	}
 }
 
 class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.type = 'magazine';
-    }
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'magazine';
+	}
 }
 
 class Book extends PrintEditionItem {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount)
-        this.author = author;
-        this.type = 'book';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount)
+		this.author = author;
+		this.type = 'book';
+	}
 }
 
 class NovelBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount)
-        this.type = 'novel';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount)
+		this.type = 'novel';
+	}
 }
 
 class FantasticBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount)
-        this.type = 'fantastic';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount)
+		this.type = 'fantastic';
+	}
 }
 
 class DetectiveBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount)
-        this.type = 'detective';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount)
+		this.type = 'detective';
+	}
 }
 
 class Library {
-    constructor(name, books) {
-        this.name = name;
-        this.books = [];
-    }
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
 
-    addBook(book, magazine) {
-        if (this.state > 30) {
-            this.books.push(book);
-        }
-    }
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
 
-    findBookBy(name, releaseDate, pagesCount, author, type, searchBook) {
-        if (searchBook) {
-            return searchBook;
-        } else {
-            return null;
-        }
-    }
+	findBookBy(type, value) {
+		for (let i = 0; i < this.books.length; i++) {
+			if (this.books[i][type] === value) {
+				return value;
+			} else {
+				return null;
+			}
+		}
+	}
 
-    giveBookByName(searchName) {
-        const bookToDelete = searchName;
-        if (searchName) {
-            const filteredBooks = this.books.filter((book) => book !== bookToDelete);
-            return searchName;
-        } else {
-            return 0;
-        }
-    }
+	giveBookByName(bookName) {
+		for (let i = 0; i < this.books.length; i++) {
+			if (this.books[i].name === bookName) {
+				delete this.books[i].name;
+				return bookName;
+			} else {
+				return null;
+			}
+		}
+	}
 }
-
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿function parseCount(number) {
+    if (parseFloat(number) === Nan) {
+        throw new Error("Невалидное значение");
+    }
+    return Number.parseFloat()
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        console.log(error);
+    } finally {
+
+    }
+}
+
+class Triangle {
+    constructor(firstSide, secondSide, thirdSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+
+        if ((firstSide + secondSide) < thirdSide) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+
+
+    get perimeter() {
+        let perimeter = firstSide + secondSide + thirdSide;
+        return perimeter.toFixed(3);
+    }
+
+    get area() {
+        let halfPerimeter = this.perimeter / 2;
+        let area = Math.sqrt(halfPerimeter * (halfPerimeter - firstSide) * (halfPerimeter - secondSide) * (halfPerimeter - thirdSide));
+        return area.toFixed(3);
+    }
+}
+
+function getTriangle(firstSide, secondSide, thirdSide) {
+    try {
+        return new Triangle();
+    } catch (error) {
+        console.log(error);
+    } finally {
+        return new Triangle {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
